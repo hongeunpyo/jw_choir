0a66549ec4416a03b9ae134ad32b700f1eb6233e
hongeunpyo <eunpyohong@gmail.com>
2019-03-22 15:53:24 -0400

    skeleton for redux created

---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import * as App from './components/app';
+
+ReactDOM.render(<App/>, document.getElementById('app') as HTMLElement);
